dcbca7e29341e4b4673dbde93d5e5beefc04a0a3
Abhishek Mane <abhishekmane280@gmail.com>
2025-07-23 09:22:29 +0530

    Update Reports.sql

--- Reports.sql
+++ Reports.sql
@@ -1,9 +1,25 @@
-Full Stack python Learning
+-- 1. Total Orders per Customer
+SELECT c.name, COUNT(o.order_id) AS total_orders
+FROM Customer c
+JOIN Orders o ON c.customer_id = o.customer_id
+GROUP BY c.name;
 
-6th January Python (01PYTH23-01
+-- 2. Product-wise Sales Summary
+SELECT p.name, SUM(oi.quantity) AS total_sold
+FROM Product p
+JOIN OrderItem oi ON p.product_id = oi.product_id
+GROUP BY p.name;
 
-28th February 2023 python is done.
+-- 3. Revenue by Category
+SELECT cat.category_name, SUM(oi.unit_price * oi.quantity) AS total_revenue
+FROM Category cat
+JOIN Product p ON cat.category_id = p.category_id
+JOIN OrderItem oi ON p.product_id = oi.product_id
+GROUP BY cat.category_name;
 
-15th pending python class.
+-- 4. Daily Sales Report
+SELECT o.order_date, SUM(oi.unit_price * oi.quantity) AS daily_sales
+FROM Orders o
+JOIN OrderItem oi ON o.order_id = oi.order_id
+GROUP BY o.order_date;
 
-28th February 2023 SQL is done. 
